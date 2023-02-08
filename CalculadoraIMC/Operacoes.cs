1db69d71fe88f272d39e14d7b41891ca0184ecdc
Lucas Ribeiro <lukinhas.lr655@gmail.com>
2023-02-08 20:58:43 -0300

    Testes Back

---
+++ CalculadoraIMC/Operacoes.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraIMC
+{
+    public static class Operacoes
+    {
+        public static double CalcularIMC(double peso, double altura)
+        {
+            return Math.Round((peso / (altura * altura)), 2);
+        }
+
+        public static String ClassificarIMC(double imc)
+        {
+            if (imc < 18.5)
+            {
+                return "Abaixo do Peso";
+            }
+            // else if (imc > 18.5 && imc < 25)
+            else if (imc < 25)
+            {
+                return "Peso Normal";
+            }
+            // else if (imc > 25 && imc < 30)
+            else if (imc < 30)
+            {
+                return "Sobrepeso";
+            }
+            // else if (imc > 30 && imc < 35)
+            else if (imc < 35)
+            {
+                return "Obesidade Grau I";
+            }
+            // else if (imc > 35 && imc < 40)
+            else if (imc < 40)
+            {
+                return "Obesidade GrauII";
+            }
+            else
+            {
+                return "Obesidade Grau III";
+            }
+        }
+    }
+}
---
+++ TesteIMC/ClassificarIMCTest.cs
@@ -0,0 +1,24 @@
+﻿using CalculadoraIMC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteIMC
+{
+    [TestClass]
+    public class ClassificarIMCTest
+    {
+        [TestMethod]
+        public void ClassificarIMC()
+        {
+            double imc = 28;
+
+            var classificarIMC = Operacoes.ClassificarIMC(imc);
+
+            Assert.AreEqual("Sobrepeso", classificarIMC);
+        }
+            
+     }
+}
